63d51ed7e32496ec7bb696469c67d7f5e295296c
Ruta Jogani <ruta007jogani@gmail.com>
2026-09-04 00:10:41 +0530

    makel little changein app.jsx file

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
   return (
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-3 text-orange-500 bg-gray-800">
-        <h1 className="text-center text-white">Password Generator</h1>
+        <h2 className="text-center text-white">Password Generator</h2>
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
           <input
             type="text"
